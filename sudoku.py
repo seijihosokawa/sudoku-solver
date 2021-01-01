d51380f123ef0e4bc5799d27b8861a8390073cbe
Seiji Hosokawa <seijihosokawa@gmail.com>
2021-01-01 16:44:51 -0500

    added Sudoku solver class

--- sudoku.py
+++ sudoku.py
@@ -7,6 +7,9 @@ WIDTH = HEIGHT = MARGIN * 2 + SIDE * 9
 
 
 class SudokuUI(Frame):
+    """
+    This class handles the Sudoku UI using tkinter, creates grid and draws the puzzle
+    """
     def __init__(self, parent, game):
         Frame.__init__(self, parent)
         self.canvas = Canvas(self, width=500, height=500)
@@ -19,8 +22,10 @@ class SudokuUI(Frame):
         self.parent.title("Sudoku")
         self.pack(fill=BOTH, expand=1)
         self.canvas.pack(fill=BOTH, side=TOP)
-        clear_button = Button(self, text="Clear answers", command=self.__clear_answers)
+        clear_button = Button(self, text="Clear answers", command=self.__clear_answers, highlightbackground="gray")
         clear_button.pack(fill=BOTH, side=BOTTOM)
+        solve_button = Button(self, text="Solve Sudoku", command=self.__solver_helper, highlightbackground="gray")
+        solve_button.pack(fill=BOTH, side=BOTTOM)
         self.__draw_grid()
         self.__draw_puzzle()
         self.canvas.bind("<Button-1>", self.__cell_clicked)
@@ -87,7 +92,11 @@ class SudokuUI(Frame):
                 self.__end_game()
 
     def __clear_answers(self):
-        self.game.reset()
+        # reset puzzle
+        self.__draw_puzzle()
+
+    def __solver_helper(self):
+        SudokuSolver(self.game.board)
         self.__draw_puzzle()
 
     def __end_game(self):
@@ -96,7 +105,12 @@ class SudokuUI(Frame):
 
 
 class SudokuGame(list):
+    """
+    This class is used to check whether or not the Sudoku puzzle is correct, checks if win by checking column, row and
+    block to see if values 1-9 are present
+    """
     def __init__(self, board_file):
+        super().__init__()
         self.board = board_file
         self.game_over = False
 
@@ -129,6 +143,50 @@ class SudokuGame(list):
             for r in range(row * 3, (row + 1) * 3)
             for c in range(column * 3, (column + 1) * 3)
         ])
+
+
+class SudokuSolver(list):
+    """
+    This class will be used to find if a solution is possible using backtracking algorithm
+    """
+    def __init__(self, board_file):
+        super().__init__()
+        self.solve_sudoku(board_file)
+
+    def find_next_empty(self, board):
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == 0:
+                    return r, c
+        return None, None
+
+    def is_valid(self, board, guess, row, col):
+        row_values = board[row]
+        if guess in row_values:
+            return False
+        col_values = [board[i][col] for i in range(9)]
+        if guess in col_values:
+            return False
+        row_start = (row // 3) * 3
+        col_start = (col // 3) * 3
+        for r in range(row_start, row_start+3):
+            for c in range(col_start, col_start+3):
+                if board[r][c] == guess:
+                    return False
+        return True
+
+    def solve_sudoku(self, board):
+        row, col = self.find_next_empty(board)
+        if row is None:
+            return True
+        for guess in range(1, 10):
+            if self.is_valid(board, guess, row, col):
+                # update board
+                board[row][col] = guess
+                if self.solve_sudoku(board):
+                    return True
+            board[row][col] = 0
+        return False
 
 
 if __name__ == '__main__':
